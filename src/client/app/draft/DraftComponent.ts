a8a2ae1c3961b854582a86156595ec8c6c4b178b
kwiersma <kwiersma@gmail.com>
2016-05-30 18:42:11 -0500

    Added a draft order component

--- src/client/app/draft/DraftComponent.ts
+++ src/client/app/draft/DraftComponent.ts
@@ -1,21 +1,25 @@
 import {Component, OnInit} from 'angular2/core';
 import {RouterLink} from 'angular2/router';
 import {PlayerListComponent} from "../players/PlayerListComponent";
+import {DraftOrderComponent} from "./DraftOrderComponent";
+import {FantasyTeam} from "../shared/model/model";
+import {FantasyTeamService} from "../shared/services/FantasyTeamService";
 
 @Component({
     selector: 'draft',
     templateUrl: 'app/draft/draft.component.html',
-    directives: [RouterLink, PlayerListComponent]
+    directives: [RouterLink, PlayerListComponent, DraftOrderComponent]
 })
-
 export class DraftComponent implements OnInit {
 
-    constructor() {
+    teams: FantasyTeam[];
 
-    }
+    constructor(private fantasyTeamService: FantasyTeamService) {}
 
     ngOnInit() {
-
+        this.fantasyTeamService.getFantasyTeams()
+            .subscribe((response: FantasyTeam[]) => {
+                this.teams = response;
+            });
     }
-
 }
