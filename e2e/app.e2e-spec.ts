b3202e0fc6066bf8333210e17d2bb233dcc3ca12
Kurt Wiersma <kwiersma@gmail.com>
2017-06-03 16:09:15 -0500

    Upgraded to Angular CLI 1.1.0

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,10 @@ describe('ng-new App', () => {
     page = new DjleagueNg2Home();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', done => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    page.getParagraphText()
+        .then(msg => expect(msg).toEqual('Welcome to app!!'))
+        .then(done, done.fail);
   });
 });
